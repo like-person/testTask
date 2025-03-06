fdfc8b3cbccbd5373cf9c685bfcbb845d81dba1c
Damir <like-person@mail.ru>
2025-03-06 17:36:29 +0500

    Update init.php

--- init.php
+++ init.php
@@ -17,14 +17,3 @@ function handlerUpdateUserBalance(&$arFields) {
 }
 
 
-
-require dirname(__FILE__) . '/constants.php';
-
-
-require dirname(__FILE__) . '/autoload.php';
-
-
-require dirname(__FILE__) . '/event_handler.php';
-
-
-
